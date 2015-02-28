71c872df5b0d9e0657749dd01c1496beef85bbb0
mswenson <msahat@gmail.com>
2015-02-27 17:39:52 -0800

    First Homework assignment for the mobile class in GA
    Getting a grip of the storyboarding component of Xcode - and finding
    out how fucked up the constraint tool is.

---
+++ fuckConstraints/fuckConstraints/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fuckConstraints
+//
+//  Created by Matt Swenson on 2/27/15.
+//  Copyright (c) 2015 Matt Swenson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
